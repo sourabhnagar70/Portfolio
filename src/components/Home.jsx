01beed610da3fc0b855e64294408df848b9af262
sourabhnagar70 <sourabhnagar.work>
2024-12-01 18:24:56 +0530

    modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -75,7 +75,7 @@ function Home() {
       <div className='md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1'>
       <img 
       src={pic}
-       className='rounded-full md:w-[450px] md:h-[450px]' alt="" />
+       className='rounded-full md:w-[450px] md:h-[450px]  ' alt="" />
       </div>
       </div>
       <br />
